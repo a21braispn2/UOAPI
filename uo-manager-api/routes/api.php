496bc51a17bc910349e3f355705c4f98df85fb65
a21braispn <a21braispn@iessanclemente.net>
2026-05-28 12:41:05 +0000

    feat: add API routes for home, documentation, and debug information

--- uo-manager-api/routes/api.php
+++ uo-manager-api/routes/api.php
@@ -4,6 +4,21 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('api');
+});
+
+Route::get('/docs', function () {
+    return view('docs');
+});
+
+Route::get('/debug', function () {
+    return response()->json([
+        'message' => 'Debug route is working!',
+        'timestamp' => now(),
+    ]);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
